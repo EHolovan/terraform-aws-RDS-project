32f08390bc37f0b363c42d8ab80ab50ce0b42376
maxat2416 <maksat.kurk@gmail.com>
2021-07-28 21:25:12 +0000

    added editions for module

--- example/module.tf
+++ example/module.tf
@@ -7,12 +7,12 @@ module "db" {
   engine_version = "5.6.10a"
   instance_class  = "db.r4.large"
 
-  vpc_id  = ""
-  subnets = ["", ""]
+  vpc_id  = "aws_vpc.main.id"
+  subnets = ["subnet-04ebd5261c9059397", "subnet-0e30c322b02c3ea49", "subnet-0d71383e48bb980c9", "subnet-00fcb3cfa493d7ede", "subnet-0af6be2f81390aee1", "subnet-0bb8a7fa1b01cf2a3"]
 
 //   replica_count           = 
-  allowed_security_groups = [""]
-  allowed_cidr_blocks     = [""]
+  allowed_security_groups = ["sg-0a6ed2ac663995399"]
+  allowed_cidr_blocks     = ["10.0.0.0/16"]
 
 // storage_encrypted   = true
   apply_immediately   = true
--- variable.tf
+++ variable.tf
@@ -1,3 +1,23 @@
 variable "aws_ssm_parameter_name" {
-    default = "/devops/us-east-1/passwords/team3db-cluster"
+  default = "/devops/us-east-1/passwords/team3db-cluster"
+}
+variable "instance_class" {
+  type    = string
+  default = "db.r4.large"
+}
+variable "cluster_identifier" {
+  default = "dbname"
+}
+variable "engine" {
+  default = "aurora"
+  type    = string
+}
+variable "engine_version" {
+  default = "5.6.10a"
+}
+variable "database_name" {
+  default = "dbname"
+}
+variable "master_username" {
+  default = "team3db"
 }
